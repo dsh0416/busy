27e11cf82b0f05d7292b49a165c41224ceb6fa18
Delton Ding <dsh0416@gmail.com>
2021-07-02 02:49:07 +0800

    fixes

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -13,15 +13,16 @@ class BadgesController < ApplicationController
       end
     end
 
-    resp = Faraday.get 'https://img.shields.io/static/v1', {
-      label: 'Busy Status',
-      message: status,
-      color: color,
-    }
-
     response.headers["Cache-Control"] = "no-cache, no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
-    send_data resp.body, :filename => 'status.svg', :type => 'image/svg+xml'
+
+    query = {
+      label: 'Busy Status',
+      message: status,
+      color: color,
+      style: params[:style] || 'for-the-badge',
+    }.to_query
+    redirect_to "https://img.shields.io/static/v1?#{query}" 
   end
 end
