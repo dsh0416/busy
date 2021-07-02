329ea401a695768e3eaec70670332f3b7ee01ed9
Delton Ding <dsh0416@gmail.com>
2021-07-02 13:41:21 +0800

    use reverse proxy

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -13,16 +13,17 @@ class BadgesController < ApplicationController
       end
     end
 
-    response.headers["Cache-Control"] = "no-cache, no-store"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
-
-    query = {
+    resp = Faraday.get "https://img.shields.io/static/v1", {
       label: 'Busy Status',
       message: status,
       color: color,
       style: params[:style] || 'for-the-badge',
-    }.to_query
-    redirect_to "https://img.shields.io/static/v1?#{query}" 
+    }
+
+    response.headers["Cache-Control"] = "no-cache,max-age=0,no-store,s-maxage=0,proxy-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
+
+    send_data resp.body, filename: 'status.svg', type: 'image/svg+xml'
   end
 end
